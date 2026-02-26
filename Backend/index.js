d310dfa1c39ecdfad92ac3601519cbf971b46f42
akansh2416 <akasharma.6192@gmail.com>
2026-02-26 20:29:07 +0000

    containerized mern ims

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`)
 })
 
